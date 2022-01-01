20b016858aeb9d2a703bb0526b057b5c5b9008d2
ahmedamine12 <76753712+ahmedamine12@users.noreply.github.com>
2022-01-01 13:49:25 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -7,7 +7,7 @@
 #include <map>
 using namespace std ;
 #include <cstdlib>
-#define nbr_sommet 9
+#define nbr_sommet 5
 // Pour trouver la distance minimale
 int Trouver_DIST_Min(vector<int>dist, map<int,bool> passer)
 {
@@ -80,15 +80,11 @@ int main()
 
     vector<int> d ;
     // Let us create the example graph discussed above
-    int graph[nbr_sommet][nbr_sommet] = { { 0, 4, 0, 0, 0, 0, 0, 8, 0 },
-                                          { 4, 0, 8, 0, 0, 0, 0, 11, 0 },
-                                          { 0, 8, 0, 7, 0, 4, 0, 0, 2 },
-                                          { 0, 0, 7, 0, 9, 14, 0, 0, 0 },
-                                          { 0, 0, 0, 9, 0, 10, 0, 0, 0 },
-                                          { 0, 0, 4, 14, 10, 0, 2, 0, 0 },
-                                          { 0, 0, 0, 0, 0, 2, 0, 1, 6 },
-                                          { 8, 11, 0, 0, 0, 0, 1, 0, 7 },
-                                          { 0, 0, 2, 0, 0, 0, 6, 7, 0 } };
+    int graph[nbr_sommet][nbr_sommet] = { { 0, 3, 0, 2, 0 },
+                                          { 4, 0, 8, 0, 0 },
+                                          { 0, 8, 0, 5, 0 },
+                                          { 7, 1, 7, 0, 9 },
+                                          { 0, 0, 1, 9, 0} };
 
     d= dijkstra(graph, 0);
 
